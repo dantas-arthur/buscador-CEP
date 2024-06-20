bdc40dbf7e3074bb61c3ae2370f593d4868924ee
LonelyALpHaz <dantas.arthur.contact@gmail.com>
2024-06-20 17:42:17 -0300

    Map added

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // API request
-
 function getCEP() {
     // Get hint and cep data HTML elements
     var helpText = document.getElementById("help");
@@ -27,7 +26,7 @@ function getCEP() {
         const bandeira = document.createElement("img");
         bandeira.src = `assets/bandeiras/${data.uf}.png`;
         bandeira.alt = `Bandeira do estado de ${data.uf}`;
-        bandeira.style.width = "20px"
+        bandeira.style.width = "25px"
         bandeira.style.marginLeft = "5px"
 
         // Display formated data
@@ -38,11 +37,33 @@ function getCEP() {
         uf.innerHTML = `<mark class="blue">UF:</mark> ${data.uf}`;
         uf.appendChild(bandeira);
         ibge.innerHTML = `<mark class="blue">IBGE:</mark> ${data.ibge}`;
-        ddd.innerHTML = `<mark class="blue">DDD:</mark> ${data.ddd}`;
-
+        ddd.innerHTML = `<mark class="blue">DDD:</mark> 0${data.ddd}`;
+    
         // Remove hint text from screen and add cep data
         helpText.style.display = "none";
         cepData.style.display = "block";
+
+        // Map latitude and longitude function
+        const logradouroEncode = encodeURIComponent(data.logradouro);
+        const bairroEncode = encodeURIComponent(data.bairro);
+        const localEncode = encodeURIComponent(data.localidade);
+        const ufEncode = encodeURIComponent(data.uf);
+        const url = `https://nominatim.openstreetmap.org/search?q=${logradouroEncode}+${bairroEncode}+${localEncode}+${ufEncode}&format=json&polygon=1&addressdetails=1`;
+        console.log(url)
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+
+            // Map with current CEP location
+            var map = L.map('map').setView([data[0].lat, data[0].lon], 13);
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+            }).addTo(map);
+            
+            // Map marker
+            var marker = L.marker([data[0].lat, data[0].lon]).addTo(map);
+        })
     })
     .catch(error => {
         window.alert("Erro ao localizar CEP. Tente novamente.")
